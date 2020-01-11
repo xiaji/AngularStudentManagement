52a1a5f533b08cabf30a05fe8dfc33829494febd
xiaji <jwjwei@gmail.com>
2020-01-11 14:48:07 +0800

    please add your own server ip adrress to webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -55,7 +55,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `'http://18.222.13.100:8080/'`
+                SERVER_API_URL: `'http://please add your instance IPv4 public ip address/'`
             }
         }),
         new CopyWebpackPlugin([
